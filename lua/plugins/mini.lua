4f3ebbf60b3077fd367541586445fb509addce62
Fabian Junkert <fabian.junkert@aoe.com>
2026-04-29 17:01:02 +0200

    refactor: split config into sub configs

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,23 @@
+return {
+  'nvim-mini/mini.nvim',
+  config = function()
+    require('mini.ai').setup {
+      mappings = {
+        around_next = 'aa',
+        inside_next = 'ii',
+      },
+      n_lines = 500,
+    }
+
+    require('mini.files').setup()
+
+    require('mini.surround').setup()
+
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+  end,
+}
